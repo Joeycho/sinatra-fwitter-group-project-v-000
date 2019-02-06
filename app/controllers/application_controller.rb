17168856c19d55c3408cacc01f85ba3780480b3e
Joey Cho <cjy12182000@naver.com>
2019-02-06 08:39:47 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,34 @@ class ApplicationController < Sinatra::Base
     enable :sessions
     set :session_secret, "password_security"
   end
+  
+  get '/' do
+    erb :index
+  end
+  
+  get '/signup' do
+    if session[:user_id]==nil
+    erb :signup
+    else
+    redirect "/tweets"
+    end
+  end
+  
+  post '/signup' do
+      if params[:username]!="" && params[:email]!="" && params[:password]!=""
+        user = User.new(:username => params[:username],:email => params[:email], :password => params[:password])
+        user.save
+        session[:user_id] = user.id
+        redirect "/tweets"
+      else
+        redirect "/signup"
+      end
+  end
+  
+  get '/tweets' do
+    
+    erb :"/tweets/index"
+  end
+  
 
 end
