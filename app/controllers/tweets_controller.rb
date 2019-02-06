1a28fda8a6c5e85ed12c68cb9190be138c95afda
Joey Cho <cjy12182000@naver.com>
2019-02-06 09:45:54 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,29 +1,33 @@
 class TweetsController < ApplicationController
-
-get '/tweets' do
-    if session[:user_id]==nil
-      redirect "/login"
-    else
-      erb :"/tweets/index"
-    end
-end
-
-get '/tweets/new' do
-  if session[:user_id]==nil
-      redirect "/login"
-    else
-      erb :"/tweets/new"
-    end
-end
-
-post '/tweets' do
-      if params[:content]!=""
-      Tweet.create(:content => params[:content], :user_id => session[:user_id])
-        redirect "/tweets"
+  
+  get '/tweets' do
+      if session[:user_id]==nil
+        redirect "/login"
       else
-        redirect "/tweets/new"
+        erb :"/tweets/index"
       end
   end
+  
+  get '/tweets/new' do
+    if session[:user_id]==nil
+        redirect "/login"
+      else
+        erb :"/tweets/new"
+      end
+  end
+  
+  post '/tweets' do
+        if params[:content]!=""
+        Tweet.create(:content => params[:content], :user_id => session[:user_id])
+          redirect "/tweets"
+        else
+          redirect "/tweets/new"
+        end
+  end
 
+  get "/tweets/:id" do
+    @tweet = Tweet.find(params[:id])
+    erb :"/tweets/show"
+  end
 
 end
