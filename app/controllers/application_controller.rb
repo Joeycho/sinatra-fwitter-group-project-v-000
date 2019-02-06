8b12508db9388583a94164f3002ce837c87e465b
Joey Cho <cjy12182000@naver.com>
2019-02-06 09:23:21 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,13 +33,6 @@ class ApplicationController < Sinatra::Base
       end
   end
   
-  get '/tweets' do
-    if session[:user_id]==nil
-    redirect "/login"
-    else
-    erb :"/tweets/index"
-    end
-  end
   
   get '/login' do
     if session[:user_id]==nil
